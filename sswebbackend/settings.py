51d19370a121d8f21bbb6d2965a40297ea2b89fb
Sabith-asp <abdullasabithpulli@gmail.com>
2024-03-22 00:24:11 +0530

    Added my project

--- sswebbackend/settings.py
+++ sswebbackend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x7kjcoy(1^poo@!yhn%)agc3c&9t)0re@t)ewaxt+(+p(7&jw-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','localhost','127.0.0.1']
 
 
 # Application definition
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'sswebbackend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sullam_db',
+        'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',    # or your database host
-        'PORT': '5433'            # or your database port
+        'PASSWORD': 'xYXLlLiJhbtUFDhnqGUuunWLeadwOxIZ',
+        'HOST': 'viaduct.proxy.rlwy.net',    # or your database host
+        'PORT': '28537'            # or your database port
     }
 }
 
@@ -124,6 +124,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = '/media/'
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
